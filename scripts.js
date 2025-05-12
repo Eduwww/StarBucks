e7c2994122a96ed557617b7227a0de18e228561e
Eduardo <146273951+Eduwww@users.noreply.github.com>
2025-05-12 16:57:19 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let circulo = document.querySelector(".circulo");
+
+function trocarCor(cor) {
+    circulo.style.backgroundColor = cor;
+}
+
+function trocaImagem(copo) {
+    document.getElementById('produto-img').src = copo;
+}
